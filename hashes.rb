60fa1287f6b34385a4ec03d17996c60bae10b1af
james <thuranira611@gmail.com>
2023-07-18 08:40:20 +0300

    print values in the hash by specifying the keys

---
+++ hashes.rb
@@ -0,0 +1,8 @@
+area_code = {
+    "Kenya" => "+254",
+    "United kingdom" => "+44",
+    "United states" => "+1",
+    "Nigeria" => "+234"
+}
+puts area_code ["Kenya"]
+puts area_code ["United kingdom"]
